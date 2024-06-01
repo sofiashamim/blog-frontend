dbb8681ab600e7d4642a601d39bf3868e354c30c
sofiashamim <sofiashamim100@gmail.com>
2024-06-02 02:52:42 +0530

    css

--- src/components/Sidebar.js
+++ src/components/Sidebar.js
@@ -3,7 +3,7 @@ import React from 'react'
 const Sidebar = (props) => {
   return (
     <div>
-      <button onClick={()=>props.setclicked(true)} className='mt-3 btn btn-info'>Create blog</button>
+      <button id='createbutton' onClick={()=>props.setclicked(true)} className='mt-3 btn btn-info'>Create blog</button>
     </div>
   )
 }
